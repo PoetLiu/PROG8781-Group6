9efc408404499ceb73592693752243abdf065b64
AlbertLiu <457566268@qq.com>
2024-11-21 14:59:56 -0500

    Fix navbar active

--- style-me/src/MyNavbar.js
+++ style-me/src/MyNavbar.js
@@ -5,8 +5,15 @@ import Navbar from 'react-bootstrap/Navbar';
 import logo from './images/logo.png';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { useLocation } from "react-router-dom";
+
+
 
 function MyNavbar() {
+  const location = useLocation();
+  const checkAlive = (self) => 
+    location.pathname === "/" + self ? "active" : ""
+  ;
   return (
     <Navbar expand="lg" className="bg-body-tertiary" bg="light" data-bs-theme="light">
       <Container>
@@ -20,7 +27,7 @@ function MyNavbar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto justify-content-center">
-            <Nav.Link href="/Digital-Fashion">Digital Fashion</Nav.Link>
+            <Nav.Link href="/Digital-Fashion" className={checkAlive("Digital-Fashion")}>Digital Fashion</Nav.Link>
             <Nav.Link href="/">Link</Nav.Link>
             <Nav.Link href="/">Link</Nav.Link>
             <Nav.Link href="/">Link</Nav.Link>
